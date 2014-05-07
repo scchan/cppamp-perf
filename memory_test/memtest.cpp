492c58611424b1c7c8004b43dd3c4ecd7488115c
scchan <siuchi.chan@amd.com>
2014-05-06 21:47:21 -0400

    adding Makefile

--- memory_test/memtest.cpp
+++ memory_test/memtest.cpp
@@ -1,14 +1,13 @@
 
 
-
 #include <cstdlib>
 #include <ctime>
 #include <cstdio>
+#include <cstring>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
-
-#define DEFAULT_NUM 1048576
 
 
 #ifdef ENABLE_CPPAMP
@@ -35,6 +34,8 @@ using namespace concurrency;
   #define SQRTF(x)  sqrt(x)
 #endif
 
+
+#include "StackTimer.hpp"
 
 
 #ifdef ENABLE_CPPAMP
@@ -89,6 +90,67 @@ int arrayHostToDeviceBandwidth(std::vector<float>& v) {
 #endif
 
 
+
+struct unique_number {
+  int current;
+  unique_number() { current=0; };
+  float operator()() {  return current++; };
+};
+
+struct output_ref {
+  int current;
+  float rand;
+  output_ref(float r=0.0f) { current=0; rand = r; };
+  bool operator()(float f) { bool b = fabs((current+rand)-f) > 0.001f; current++; return b;} 
+};
+
+
+
+#define DEFAULT_MEM_SIZE    16
+#define DEFAULT_ITERATIONS  1
+
+class Arg {
+public:
+  Arg(int argc, char** argv):argc(argc),argv(argv)
+    ,memSizeMB(DEFAULT_MEM_SIZE)
+    ,iterations(DEFAULT_ITERATIONS)
+  {
+
+    for (int n = 1; n < argc; n++) {
+      if (strcmp(argv[n],"-m")==0) {
+        n++;
+        if (n < argc) {
+          memSizeMB=atoi(argv[n]);
+        }
+        else {
+          usage();
+          exit(1);
+        }
+      }
+      else if (strcmp(argv[n],"-i")==0) {
+        if (!(++n<argc))
+          break;
+        iterations = atoi(argv[n]);
+      }
+    }
+  };
+
+  void usage() {
+    std::cout << argv[0] << " -m <memory size> -i <iterations>" << std::endl;
+  };
+
+  unsigned int memSizeMB;
+  unsigned int iterations;
+
+private:
+  int argc;
+  char** argv;
+};
+
+
+
+int main(int argc, char* argv[]) {
+
 enum {
   MEMTEST_HOST_TO_DEVICE_TEST = 0
 
@@ -105,24 +167,9 @@ enum {
   ,MEMTEST_LAST
 } MemTests;
 
-struct unique_number {
-  int current;
-  unique_number() { current=0; };
-  float operator()() {  return current++; };
-};
-
-struct output_ref {
-  int current;
-  float rand;
-  output_ref(float r=0.0f) { current=0; rand = r; };
-  bool operator()(float f) { bool b = fabs((current+rand)-f) > 0.001f; current++; return b;} 
-};
-
-int main(int argc, char* argv[]) {
-  unsigned int memSizeMB = 64*(1024*1024);
-  unsigned int numElements = memSizeMB/sizeof(float);
-  unsigned int numIter = 10;
-
+  Arg parser(argc,argv);
+  unsigned int numElements = parser.memSizeMB/sizeof(float);
+  unsigned int numIter = parser.iterations;
   srand(time(NULL));
 
   std::vector<float> v(numElements);
@@ -135,12 +182,16 @@ int main(int argc, char* argv[]) {
         t != MEMTEST_DEVICE_TO_HOST_TEST; t++) {
 
     switch(t) {
+
+#ifdef ENABLE_CPPAMP
       case ARRAYVIEW_HOST_TO_DEVICE:
         hostToDeviceTest = arrayViewHostToDeviceBandwidth;
       break;
       case ARRAY_HOST_TO_DEVICE:
         hostToDeviceTest = arrayHostToDeviceBandwidth;
       break;
+#endif
+
       default:
       break;
     };
@@ -159,12 +210,16 @@ int main(int argc, char* argv[]) {
         t != MEMTEST_LAST; t++) {
 
     switch(t) {
+
+#ifdef ENABLE_CPPAMP
       case ARRAYVIEW_DEVICE_TO_HOST:
         deviceToHostTest = arrayViewDeviceToHostBandwidth;
       break;
       case ARRAY_DEVICE_TO_HOST:
         deviceToHostTest = arrayDeviceToHostBandwidth;
       break;
+#endif
+      
       default:
       break;
     };
