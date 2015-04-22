06f9cc9ff07c7a7c635d37c81bdf5d069b01a77a
Siu Chi Chan <siuchi.chan@amd.com>
2015-04-22 18:46:00 -0400

    add array version

--- kalmar_tests/simple/simple.cpp
+++ kalmar_tests/simple/simple.cpp
@@ -2,9 +2,21 @@
 #include <cstdio>
 #include <amp.h>
 
+
+
+
+#ifdef USE_RESTRICT
+#define RESTRICT_AMP restrict(amp)
+#else
+#define RESTRICT_AMP
+#endif
+
 using namespace concurrency;
 
 #define NUM 100
+
+
+#ifdef USE_ARRAY_VIEW
 
 void add(float* a, float* b, float* c, unsigned int num) {
  
@@ -13,12 +25,42 @@ void add(float* a, float* b, float* c, unsigned int num) {
   array_view<float> cc(num,c);
  
   for (int i = 0; i < 10; i++) {
-    parallel_for_each(extent<1>(num), [=](index<1> idx) {
+    parallel_for_each(extent<1>(num), [=](index<1> idx) RESTRICT_AMP {
       cc[idx] = aa[idx] + bb[idx];
     });
   }
 
 }
+
+#else
+
+void add_array(float* a, float* b, float* c, unsigned int num) {
+
+  accelerator accelerator(accelerator::default_accelerator);
+  a
+
+  array<float> aa(num);
+  array<float> bb(num);
+  array<float> cc(num);
+ 
+  completion_future fa = copy_async(a, a+num, aa);
+  completion_future fb = copy_async(b, b+num, bb);
+
+  fa.wait();
+  fb.wait();
+
+  for (int i = 0; i < 10; i++) {
+    parallel_for_each (extent<1>(num), [=,&aa,&bb,&cc](index<1> idx) RESTRICT_AMP {
+      cc[idx] = aa[idx] + bb[idx];
+    });
+  }
+
+  copy_async(cc,c).wait();
+  return;
+}
+
+#endif
+
 
 int main(void) { 
   float a[NUM];
@@ -30,8 +72,12 @@ int main(void) {
     b[i] = i * 10.0f;
   }
 
+#ifdef USE_ARRAY_VIEW
   add(a,b,c,NUM);
-  
+#else
+  add_array(a,b,c,NUM);
+#endif
+
   int errors = 0;
   for (unsigned int i = 0; i < NUM; i++) {
     if (c[i] != (a[i]+b[i]))
