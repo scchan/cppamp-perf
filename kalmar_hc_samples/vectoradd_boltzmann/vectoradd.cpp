9fe8a44fafdd3289e59f5ec125accbaad2340066
scchan <siuchi.chan@amd.com>
2015-09-10 07:07:42 -0500

    update vector add boltzmann example

--- kalmar_hc_samples/vectoradd_boltzmann/vectoradd.cpp
+++ kalmar_hc_samples/vectoradd_boltzmann/vectoradd.cpp
@@ -1,11 +1,13 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <cassert>
 
 #include <hc.hpp>
 #include "hsa.h"
 #include <kalmar_runtime.h>
 
+#define HSA_ASSERT(X) (assert((X)==HSA_STATUS_SUCCESS))
 
 #define NUM 1000
 
@@ -26,11 +28,42 @@ int main() {
     std::generate(vc.begin(), vc.end(), [&]() { return *ia++ + *ib++; });
 #endif
 
-  Kalmar::KalmarContext* context = Kalmar::getContext();
-  Kalmar::KalmarDevice* device = context->getDevice();
-  std::wcout << L"Device: " << device->get_description() << std::endl;
+  hc::accelerator acc;
+  hc::accelerator_view av = acc.get_auto_selection_view();
 
+  //Kalmar::KalmarContext* context = Kalmar::getContext();
+  //Kalmar::KalmarDevice* device = context->getDevice();
+  //std::wcout << L"Device: " << device->get_description() << std::endl;
 
+  //hsa_region_t mem_region = device->getHSAAMRegion();
+
+  hsa_region_t* mem_region = static_cast<hsa_region_t*>(av.get_hsa_am_region());
+  hsa_agent_t* agent = static_cast<hsa_agent_t*>(av.get_hsa_agent());
+
+  int *device_a, *device_b, *device_c;
+
+  HSA_ASSERT(hsa_memory_allocate(*mem_region, NUM*sizeof(int), (void**)&device_a));
+  HSA_ASSERT(hsa_memory_assign_agent(device_a, *agent, HSA_ACCESS_PERMISSION_RW));
+
+  HSA_ASSERT(hsa_memory_allocate(*mem_region, NUM*sizeof(int), (void**)&device_b));
+  HSA_ASSERT(hsa_memory_assign_agent(device_b, *agent, HSA_ACCESS_PERMISSION_RW));
+
+  HSA_ASSERT(hsa_memory_allocate(*mem_region, NUM*sizeof(int), (void**)&device_c));
+  HSA_ASSERT(hsa_memory_assign_agent(device_c, *agent, HSA_ACCESS_PERMISSION_RW));
+
+  HSA_ASSERT(hsa_memory_copy(device_a, va.data(), NUM*sizeof(int)));
+  HSA_ASSERT(hsa_memory_copy(device_b, vb.data(), NUM*sizeof(int)));
+
+  hc::parallel_for_each(hc::extent<1>(NUM),[&](hc::index<1> idx) __attribute((hc)) {
+      int i = idx[0];
+      device_c[i] = device_a[i] + device_b[i];
+  }).wait();
+
+  HSA_ASSERT(hsa_memory_copy(vc.data(), device_c, NUM*sizeof(int)));
+  
+  HSA_ASSERT(hsa_memory_free(device_a));
+  HSA_ASSERT(hsa_memory_free(device_b));
+  HSA_ASSERT(hsa_memory_free(device_c));
 
   int errors = 0;
   auto ia = va.begin();
